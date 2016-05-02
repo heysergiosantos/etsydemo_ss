86ecfa4ced13ebf574fa097adb0ecff6ee9d9415
Sergio Santos <pserssantos@gmail.com>
2016-05-02 17:19:02 +0100

    Tie listings to Users

--- etsydemo/db/schema.rb
+++ etsydemo/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160502100955) do
+ActiveRecord::Schema.define(version: 20160502150441) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160502100955) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
 # Could not dump table "users" because of following NoMethodError
