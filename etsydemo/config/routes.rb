cbc682a6f3413fafed105c810fd6bc0f1c490603
Sergio Santos <pserssantos@gmail.com>
2016-05-03 23:36:39 +0100

    added connection of users to listings and home banner

--- etsydemo/config/routes.rb
+++ etsydemo/config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
-  resources :listings
-
-  get 'pages/about'
-
-  get 'pages/contact'
-
   root 'listings#index'
+  resources :listings
+  get 'seller' => "listings#seller"
 
   devise_for :users
+
+  get 'pages/about'
+  get 'pages/contact'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
